dbe097003546aebece0f059f5b024754d1f137a5
Eron Lu <eron3759225@gmail.com>
2022-10-28 00:30:33 +0800

    test img

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ const Cards = () => {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="react-rwd-website/images/img-9.jpg"
+              src="./react-rwd-website/images/img-9.jpg"
               text="Explore the hidden waterfall deep"
               label="Adventure"
               paht="/services"
             />
             <CardItem
-              src="react-rwd-website/images/img-2.jpg"
+              src="./react-rwd-website/images/img-2.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               paht="/services"
@@ -24,19 +24,19 @@ const Cards = () => {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="react-rwd-website/images/img-6.jpg"
+              src="./react-rwd-website/images/img-6.jpg"
               text="Guess u dont notice this text"
               label="City"
               paht="/services"
             />
             <CardItem
-              src="react-rwd-website/images/img-4.jpg"
+              src="./react-rwd-website/images/img-4.jpg"
               text="English has better look in this area."
               label="Sport"
               paht="/services"
             />
             <CardItem
-              src="react-rwd-website/images/img-8.jpg"
+              src="./react-rwd-website/images/img-8.jpg"
               text="Must come, because sand is fun, or u can just take a rock pet."
               label="Sand"
               paht="/services"
