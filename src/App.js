322934001c3678c3b24558ac186ba4d03b43f84b
Eron Lu <eron3759225@gmail.com>
2022-10-28 00:20:04 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/services" element={<Services />} />
